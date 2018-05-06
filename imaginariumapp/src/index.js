8917a50612a5ad7b8980a201f58c61d9b9af3133
lapinell <lapinell@gmail.com>
2018-05-06 17:30:33 -0500

    created XHR call via axios

--- imaginariumapp/src/index.js
+++ imaginariumapp/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import UserData from './getDatabase.js';
+
+UserData();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
