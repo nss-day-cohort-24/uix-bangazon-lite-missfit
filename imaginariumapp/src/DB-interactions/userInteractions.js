34a730c65fdaa48d0a44f6372e13986941ef1e79
lapinell <lapinell@gmail.com>
2018-05-06 18:10:33 -0500

    compiled all user-database interactions into one folder

---
+++ imaginariumapp/src/DB-interactions/userInteractions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import DatabaseURL from './getDatabase';
+
+export const CreateUser = (userEmail, userPassword) => {
+    axios.post(DatabaseURL('users/'), {
+        "email": userEmail,
+        "password": userPassword
+    })
+    .then(function(response) {
+        console.log('we added the user:', response);
+    })
+    .catch(function(error){
+        console.log('we ran into an error:',  error);
+    })
+};
+
+export const DeleteUser = (userid) => {
+    axios.delete(
+        DatabaseURL('users/'+ userid + '/')
+    )
+    .then(function(response){
+        console.log('user was deleted: ', response);
+    })
+    .catch( function(error) {
+        console.log('there was an error deleting the user:', error);
+    })
+};
