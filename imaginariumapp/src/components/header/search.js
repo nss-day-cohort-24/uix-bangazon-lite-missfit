b3ceb3e9e2e5895fc971ed5d31a32c441c350789
lapinell <lapinell@gmail.com>
2018-05-08 13:51:47 -0500

    trying to add styling

--- imaginariumapp/src/components/header/search.js
+++ imaginariumapp/src/components/header/search.js
@@ -2,11 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 
-export default function NavSearch () {
+
     return (
     <form className="form-inline">
         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button styles={styles} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> 
     );
 };
