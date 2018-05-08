9c5fd5d3c3ab096062684b0733d24a6e97e481f8
lapinell <lapinell@gmail.com>
2018-05-08 10:59:30 -0500

    created and styled Logo component

--- imaginariumapp/src/components/header/logo.js
+++ imaginariumapp/src/components/header/logo.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import WebFont from 'webfontloader';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 
-export default function Logo () {
+WebFont.load({
+    google: {
+        families: ['Crimson Text']
+    }
+});
+
+export default function Logo ({color, size, transform}) {
+    const styles = {
+        color,
+        fontSize: Logo.sizes[size],
+        'text-transform': 'uppercase',
+        'font-family': 'Crimson Text'
+    };
+
     return (
-        <a className="navbar-brand" href="./"><h1>Imaginarium</h1></a>
+        <a className="navbar-brand logo" style={styles} href="./"><h1 >Imaginarium</h1></a>
     );
+};
+
+Logo.propTypes = {
+    /* The color of the Logo */
+    color: PropTypes.string,
+    /* The Size of the Logo */
+    size: PropTypes.oneOf(['small', 'normal', 'large'])
+};
+
+Logo.defaultProps = {
+    color: 'rgb(208,168,10)',
+    size: 'normal'
+};
+
+Logo.sizes = {
+    small: '14px',
+    normal: '20px',
+    large: '28px'
 };
