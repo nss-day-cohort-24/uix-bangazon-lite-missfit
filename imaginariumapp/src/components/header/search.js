46cfffe6aa350036925e6fdc94fefb9b06d6e8be
lapinell <lapinell@gmail.com>
2018-05-08 13:52:18 -0500

    removed styling for now

--- imaginariumapp/src/components/header/search.js
+++ imaginariumapp/src/components/header/search.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap-grid.css';
     return (
     <form className="form-inline">
         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button styles={styles} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> 
     );
 };
