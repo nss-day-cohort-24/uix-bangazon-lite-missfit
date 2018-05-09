6fc0ce1f68a9272b04147ec46c5184b2215ad219
lapinell <lapinell@gmail.com>
2018-05-09 09:59:45 -0500

    added new call to action button

--- imaginariumapp/src/components/homePage/jumbotron.js
+++ imaginariumapp/src/components/homePage/jumbotron.js
@@ -10,7 +10,8 @@ export default function Jumbotron () {
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr class="my-4" />
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Create Your Account</a>
+            <a class="btn btn-primary btn-lg" href="#newProducts" role="button">Explore Our Products</a>
         </div>
     );
 };
