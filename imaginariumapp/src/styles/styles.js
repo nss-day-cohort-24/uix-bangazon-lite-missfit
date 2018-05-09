cdc84a9a9caebefabb846948052d1396682ae1d0
lapinell <lapinell@gmail.com>
2018-05-09 09:58:48 -0500

    changed folder name

---
+++ imaginariumapp/src/styles/styles.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import WebFont from 'webfontloader';
+
+export function Fonts () {
+    return (
+        WebFont.load({
+            google: {
+                families: ['CrimsonText', 'Lato']
+            }
+        })
+    );
+};
+
+export function Colors () {
+    return (
+        {
+            gold: 'rgb(208,168,10)',
+            orange: 'rgb(254,134,55)',
+            blueGray: 'rgb(44,48,54)'
+        }
+    );
+}
+
